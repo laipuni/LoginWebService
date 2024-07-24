0bc54ecf2029979133bddde9802966b9ba71a03d
laipuni <2tgb02023@gmail.com>
2024-07-24 18:14:30 +0900

    bugfix : / 없어 경로 못찾는 에러 ,java 실행 명령어 에러 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,10 +7,7 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ec2-user/app
 PROJECT_NAME=LoginWebService
 
-echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
-
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -28,7 +25,7 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME를 profile=$IDLE_PROFILE로 실행합니다."
 
 nohup java -jar \
-    -Dspring.config.location=home/ec2-user/app/application-real-db.yml,
-    /home/ec2-user/app/application-$IDLE_PROFILE.yml \
+    -Dspring.config.location=/home/ec2-user/app/application-real-db.yml,\
+     /home/ec2-user/app/application-$IDLE_PROFILE.yml \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
