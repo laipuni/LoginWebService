56ba6eacd0af9bb241bc4ad9066b392441d2ce6a
laipuni <2tgb02023@gmail.com>
2024-07-30 12:00:33 +0900

    feat : 권한 enum Role 개발

---
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.loginwebservice.loginwebservice.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST","손님"),
+    USER("ROLE_USER","일반 유저"),
+    ADMIN("ROLE_ADMIN","관리자");
+
+    private final String key;
+    private final String title;
+}
