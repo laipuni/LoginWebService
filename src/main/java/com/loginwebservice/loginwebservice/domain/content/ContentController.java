8333d024c8fd632d970a72c29f444e35d44e40f1
laipuni <2tgb02023@gmail.com>
2024-07-30 12:44:56 +0900

    feat : content 등록 api 로직 변경
    컨트롤러 계층과 서비스 계층을 사이 dto를 추가했다.

--- src/main/java/com/loginwebservice/loginwebservice/domain/content/ContentController.java
+++ src/main/java/com/loginwebservice/loginwebservice/domain/content/ContentController.java
@@ -1,9 +1,11 @@
 package com.loginwebservice.loginwebservice.domain.content;
-
-import com.loginwebservice.loginwebservice.ContentAddResponse;
 import com.loginwebservice.loginwebservice.api.ApiResponse;
 import com.loginwebservice.loginwebservice.domain.content.request.ContentAddRequest;
+import com.loginwebservice.loginwebservice.domain.content.request.ContentAddServiceRequest;
+import com.loginwebservice.loginwebservice.domain.content.response.ContentAddResponse;
 import com.loginwebservice.loginwebservice.domain.content.response.ContentListResponse;
+import com.loginwebservice.loginwebservice.security.annotation.UserInfo;
+import com.loginwebservice.loginwebservice.security.resolver.dto.LoginUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,8 +26,10 @@ public class ContentController {
 
     @PostMapping("/content")
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponse<ContentAddResponse> homeInput(@Valid @RequestBody ContentAddRequest content){
-        ContentAddResponse response = contentService.save(content);
+    public ApiResponse<ContentAddResponse> homeInput(@Valid @RequestBody ContentAddRequest content, @UserInfo LoginUser user){
+        ContentAddResponse response = contentService.save(
+                ContentAddServiceRequest.of(content, user.getEmail())
+        );
         return ApiResponse.of(HttpStatus.CREATED,response);
     }
 }
