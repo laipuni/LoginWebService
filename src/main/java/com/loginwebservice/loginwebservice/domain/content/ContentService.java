afb714857c81d10a8081892fc5b631506169a383
laipuni <2tgb02023@gmail.com>
2024-07-30 11:56:20 +0900

    feat : ContentService에 content를 등록한 유저 조회 로직 추가

--- src/main/java/com/loginwebservice/loginwebservice/domain/content/ContentService.java
+++ src/main/java/com/loginwebservice/loginwebservice/domain/content/ContentService.java
@@ -1,8 +1,10 @@
 package com.loginwebservice.loginwebservice.domain.content;
 
-import com.loginwebservice.loginwebservice.ContentAddResponse;
-import com.loginwebservice.loginwebservice.domain.content.request.ContentAddRequest;
+import com.loginwebservice.loginwebservice.domain.content.response.ContentAddResponse;
+import com.loginwebservice.loginwebservice.domain.content.request.ContentAddServiceRequest;
 import com.loginwebservice.loginwebservice.domain.content.response.ContentListResponse;
+import com.loginwebservice.loginwebservice.domain.user.User;
+import com.loginwebservice.loginwebservice.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ContentService {
 
+    private final UserRepository userRepository;
     private final ContentRepository contentRepository;
 
     @Transactional
-    public ContentAddResponse save(final ContentAddRequest request) {
-        Content content = contentRepository.save(Content.of(request.getContent()));
+    public ContentAddResponse save(final ContentAddServiceRequest request) {
+        User user = userRepository.findUserByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
+        Content content = contentRepository.save(Content.of(user,request.getContent()));
         return ContentAddResponse.of(content);
     }
 
