9dfffe8e31a3fa2606eea11f1d4932f5c856bd9a
laipuni <2tgb02023@gmail.com>
2024-07-30 12:03:02 +0900

    feat : oauth2를 통해 받아온 api를 저장하는 CustomOAuth2UserService 개발

---
+++ src/main/java/com/loginwebservice/loginwebservice/security/oauth2/CustomOAuth2UserService.java
@@ -0,0 +1,65 @@
+package com.loginwebservice.loginwebservice.security.oauth2;
+
+import com.loginwebservice.loginwebservice.domain.user.User;
+import com.loginwebservice.loginwebservice.domain.user.UserRepository;
+import com.loginwebservice.loginwebservice.security.oauth2.dto.OAuthAttributes;
+import com.loginwebservice.loginwebservice.security.oauth2.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(final OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        DefaultOAuth2UserService oauth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oauth2UserService.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        String attributeName = userRequest.getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(
+                registrationId,
+                attributeName,
+                oAuth2User.getAttributes()
+        );
+
+        User user = saveOrUpdate(attributes);
+
+        httpSession.setAttribute("user",new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getAttributesNameKey()
+        );
+    }
+
+    private User saveOrUpdate(final OAuthAttributes attributes) {
+        User user = userRepository.findUserByEmail(attributes.getEmail())
+                .map(findUser -> findUser.update(
+                        attributes.getName(), attributes.getEmail(), attributes.getPicture()
+                ))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
+}
