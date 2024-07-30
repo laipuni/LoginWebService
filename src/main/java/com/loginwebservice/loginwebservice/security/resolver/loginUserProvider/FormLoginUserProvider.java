9f4af6d2471b6ddeca205a8652eb5e6b89adeb02
laipuni <2tgb02023@gmail.com>
2024-07-30 12:26:26 +0900

    feat : FormLoginUserProvider 개발
    FormLoginUserProvider는 UserNamvePasswordAuthenticationToken 타입을 지원한다.
    해당 토큰을 LoginUser에 저장하고 반환한다.

---
+++ src/main/java/com/loginwebservice/loginwebservice/security/resolver/loginUserProvider/FormLoginUserProvider.java
@@ -0,0 +1,29 @@
+package com.loginwebservice.loginwebservice.security.resolver.loginUserProvider;
+
+import com.loginwebservice.loginwebservice.security.domain.SecurityUser;
+import com.loginwebservice.loginwebservice.security.resolver.dto.LoginUser;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+
+//form Login 방식으로 로그인하는 유저의 정보를 리졸브하는 provider
+public class FormLoginUserProvider implements AuthenticationLoginUserProvider{
+
+    @Override
+    public boolean supports(final Authentication authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication.getClass());
+    }
+
+    @Override
+    public LoginUser resolve(final Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+        if(SecurityUser.class.isAssignableFrom(principal.getClass())){
+            SecurityUser user = (SecurityUser) principal;
+            return LoginUser.of(user.getUsername(), user.getEmail(), user.getPicture());
+        } else {
+            User user = (User) principal;
+            return LoginUser.of(user.getUsername(), null,null);
+        }
+    }
+
+}
