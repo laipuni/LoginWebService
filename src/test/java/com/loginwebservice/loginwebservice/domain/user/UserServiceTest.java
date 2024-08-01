b0ea8a7a7a89a707965c221cd5f678b442d1b382
laipuni <2tgb02023@gmail.com>
2024-08-01 21:19:53 +0900

    test : userService에 회원 등록하는 기능 테스트

---
+++ src/test/java/com/loginwebservice/loginwebservice/domain/user/UserServiceTest.java
@@ -0,0 +1,90 @@
+package com.loginwebservice.loginwebservice.domain.user;
+
+import com.loginwebservice.loginwebservice.domain.user.request.UserAddRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    @BeforeEach
+    void tearUp(){
+        userRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("회원가입 요청을 받아 회원을 등록한다.")
+    @Test
+    void join(){
+        //given
+        String expectedLoginId = "아이디";
+        String expectedName = "라이푸니";
+        String expectedPassword = "비밀번호";
+        String expectedUserName = "라이푸니";
+        String expectedEmail = "email@gmail.com";
+        UserAddRequest request = UserAddRequest.builder()
+                .loginId(expectedLoginId)
+                .password(expectedPassword)
+                .userName(expectedUserName)
+                .name(expectedName)
+                .email(expectedEmail)
+                .build();
+        //when
+        userService.join(request);
+        List<User> users = userRepository.findAll();
+        //then
+        assertThat(users).hasSize(1)
+                .extracting("loginId","password","email")
+                .containsExactly(
+                        tuple(expectedLoginId,expectedPassword,expectedEmail)
+                );
+    }
+
+
+    @DisplayName("회원가입 요청을 받았을 때, 중복된 유저가 존재할 경우 에러가 발생한다.")
+    @Test
+    void joinWithDuplicatedUser(){
+        //given
+        String expectedLoginId = "아이디";
+        String expectedName = "라이푸니";
+        String expectedPassword = "비밀번호";
+        String expectedUserName = "라이푸니";
+        String expectedEmail = "email@gmail.com";
+
+        User user = User.builder()
+                .loginId(expectedLoginId)
+                .password(expectedPassword)
+                .email(expectedEmail)
+                .name(expectedName)
+                .userName(expectedUserName)
+                .role(Role.GUEST)
+                .build();
+
+        userRepository.save(user);
+
+        UserAddRequest request = UserAddRequest.builder()
+                .loginId(expectedLoginId)
+                .password(expectedPassword)
+                .name(expectedName)
+                .userName(expectedUserName)
+                .email(expectedEmail)
+                .build();
+        //when
+        //then
+        assertThatThrownBy(()->userService.join(request))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+}
