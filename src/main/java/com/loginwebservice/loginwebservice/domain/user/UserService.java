414132c4d34f393c633eaf03b39e89e6f9312c16
laipuni <2tgb02023@gmail.com>
2024-08-01 21:19:14 +0900

    feat : userService와 회원을 등록하는 join로직 개발

---
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/UserService.java
@@ -0,0 +1,29 @@
+package com.loginwebservice.loginwebservice.domain.user;
+
+import com.loginwebservice.loginwebservice.domain.user.request.UserAddRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void join(UserAddRequest request){
+        User user = User.of(
+                request.getName(),
+                request.getEmail(),
+                null,
+                Role.GUEST,
+                request.getUserName(),
+                request.getLoginId(),
+                request.getPassword()
+        );
+        User result = userRepository.save(user);
+    }
+
+}
