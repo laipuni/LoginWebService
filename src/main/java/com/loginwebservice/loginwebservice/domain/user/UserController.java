8d5d7db5861500b3eaf3033dadd759506359e5db
laipuni <2tgb02023@gmail.com>
2024-08-02 11:32:57 +0900

    feat : 회원 등록 Api기능 개발
    bind 실패 혹은 중복된 유저가 존재할 경우 회원가입 UI를 보여준다. 회원 등록 성공할 경우 로그인 화면으로 redirection 한다.

--- src/main/java/com/loginwebservice/loginwebservice/domain/user/UserController.java
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/UserController.java
@@ -1,14 +1,45 @@
 package com.loginwebservice.loginwebservice.domain.user;
 
+import com.loginwebservice.loginwebservice.domain.user.request.UserAddRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping("/users/join")
-    public String joinForm(){
+    public String joinForm(@ModelAttribute(name = "user") UserAddRequest request){
         return "user/userAddForm";
     }
 
+    @PostMapping("/users/join")
+    public String join(
+            @Valid @ModelAttribute("user") UserAddRequest request,
+            BindingResult bindingResult,
+            Model model
+    ){
+        if(bindingResult.hasErrors()){
+            return "/user/userAddForm";
+        }
+
+        //중복된 유저가 존재해 에러가 발생한 경우
+        try{
+            userService.join(request);
+        } catch (DataIntegrityViolationException exception){
+            model.addAttribute("globalError","기입된 정보 중에 일치하는 유저가 존재합니다.");
+            return "/user/userAddForm";
+        }
+
+        return "redirect:/login";
+    }
 }
