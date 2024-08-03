d16ab742efd3668272abc09be721cb2507aee807
laipuni <2tgb02023@gmail.com>
2024-08-03 19:45:23 +0900

    feat : UserRepository에 existsUserByUserName() 추가
    닉네임이 같은 유저가 존재하는지 확인하는 기능 개발

--- src/main/java/com/loginwebservice/loginwebservice/domain/user/UserRepository.java
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-
     public Optional<User> findUserByEmail(String email);
+
+    public boolean existsUserByUserName (String userName);
 }
