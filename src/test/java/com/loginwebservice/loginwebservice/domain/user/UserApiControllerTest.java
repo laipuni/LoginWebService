698917a1120eff59ef48c19f4d55f35ce9cee95e
laipuni <2tgb02023@gmail.com>
2024-08-03 20:16:56 +0900

    test : 중복된 아이디가 있는지 확인하는 Api 테스트

---
+++ src/test/java/com/loginwebservice/loginwebservice/domain/user/UserApiControllerTest.java
@@ -0,0 +1,49 @@
+package com.loginwebservice.loginwebservice.domain.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class UserApiControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+
+    @DisplayName("")
+    @Test
+    void isExistSameLoginIdUser() throws Exception {
+        //given
+        String loginId = "아이디";
+
+        Mockito.when(userService.isExistSameLoginIdUSer(loginId)).thenReturn(Boolean.TRUE);
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/" + loginId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.data").value(Boolean.TRUE));
+    }
+
+}
