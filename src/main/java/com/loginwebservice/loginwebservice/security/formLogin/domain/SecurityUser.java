0f69b53f9802139ee1db8572c94288449023a558
laipuni <2tgb02023@gmail.com>
2024-08-05 19:11:14 +0900

    feat : 인증된 유저 정보를 담는 SecurityUser 개발
    CustomUserDetailService에서 사용할 인증된 유저 정보를 담는 SecurityUser개발

---
+++ src/main/java/com/loginwebservice/loginwebservice/security/formLogin/domain/SecurityUser.java
@@ -0,0 +1,28 @@
+package com.loginwebservice.loginwebservice.security.formLogin.domain;
+
+import com.loginwebservice.loginwebservice.domain.user.Role;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Getter
+public class SecurityUser extends User {
+
+    private String email;
+    private String picture;
+
+    public SecurityUser(com.loginwebservice.loginwebservice.domain.user.User user){
+        super(user.getLoginId(), user.getPassword(), createAuthority(List.of(user.getRole())));
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+    private static List<SimpleGrantedAuthority> createAuthority(List<Role> roleList){
+        return roleList.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getKey()))
+                .toList();
+    }
+
+}
