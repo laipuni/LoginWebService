29447744820b57fbcfe1f3dfba86816cf64f752b
laipuni <2tgb02023@gmail.com>
2024-08-05 19:13:56 +0900

    feat : LoginController에 에러메세지 model에 추가하는 로직 추가
    로그인 인증 실패시 에러메세지를 model에 "errorMessage"로 담는다.

--- src/main/java/com/loginwebservice/loginwebservice/security/LoginController.java
+++ src/main/java/com/loginwebservice/loginwebservice/security/LoginController.java
@@ -1,12 +1,21 @@
 package com.loginwebservice.loginwebservice.security;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(){
+    public String login(
+            @RequestParam(required = false, name = "errorMessage")String errorMessage,
+            Model model){
+
+        if(errorMessage != null){
+            model.addAttribute("errorMessage",errorMessage);
+        }
+
         return "login/login";
     }
 }
