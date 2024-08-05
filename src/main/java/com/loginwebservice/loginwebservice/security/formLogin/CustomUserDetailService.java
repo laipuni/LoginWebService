1635051efbc075704f48636abf68ed02047f4e6c
laipuni <2tgb02023@gmail.com>
2024-08-05 19:04:42 +0900

    feat : UserDetailsServicer구현체 CustomUserDetailService 개발
    아이디로 유저를 조회해서 SecurityUser로 반환하는 Service 개발

---
+++ src/main/java/com/loginwebservice/loginwebservice/security/formLogin/CustomUserDetailService.java
@@ -0,0 +1,25 @@
+package com.loginwebservice.loginwebservice.security.formLogin;
+
+import com.loginwebservice.loginwebservice.domain.user.User;
+import com.loginwebservice.loginwebservice.domain.user.UserRepository;
+import com.loginwebservice.loginwebservice.security.formLogin.domain.SecurityUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByLoginId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 아이디의 유저가 존재하지 않습니다."));
+
+        return new SecurityUser(user);
+    }
+}
