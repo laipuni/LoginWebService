508640d031a4bb1294c5711648fd4fd2af945fb1
laipuni <2tgb02023@gmail.com>
2024-08-07 11:43:28 +0900

    feat : JpaAuditingConfig에 EnableJpaAuditing어노테이션 auditorAwareRef = "auditorAware"추가
     AuditorAware 빈을 인식하려면 빈이름을 추가해줘야한다.

--- src/main/java/com/loginwebservice/loginwebservice/jpa/config/JpaAuditingConfig.java
+++ src/main/java/com/loginwebservice/loginwebservice/jpa/config/JpaAuditingConfig.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class JpaAuditingConfig {
 
     @Bean
