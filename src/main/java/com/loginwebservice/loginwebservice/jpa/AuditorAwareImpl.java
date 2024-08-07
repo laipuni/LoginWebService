9da12d9628081b703500aa1b2ce975bc0495876d
laipuni <2tgb02023@gmail.com>
2024-08-07 11:15:29 +0900

    feat : creatBy, updateBy를 바꿀 때 사용할 AuditorAwareImpl 개발

---
+++ src/main/java/com/loginwebservice/loginwebservice/jpa/AuditorAwareImpl.java
@@ -0,0 +1,31 @@
+package com.loginwebservice.loginwebservice.jpa;
+
+import com.loginwebservice.loginwebservice.security.formLogin.domain.SecurityUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Slf4j
+public class AuditorAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken){
+            log.debug("Not found Authentication");
+            return null;
+        }
+
+        if(!authentication.isAuthenticated()){
+            log.debug("");
+            return null;
+        }
+
+        SecurityUser user = (SecurityUser) authentication.getPrincipal();
+        return Optional.of(user.getUserSeq());
+    }
+}
