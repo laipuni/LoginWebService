2af8300c6986c291f23b2d42ed28bf8e94338051
laipuni <2tgb02023@gmail.com>
2024-08-07 17:14:24 +0900

    bugfix : JpaAuditingConfig에 auditorProvider Bean 제거
    travis에서만 ConfilctionBeanException이 발생하면서 auditorProvider를 제거해볼 것이다.

--- src/main/java/com/loginwebservice/loginwebservice/jpa/config/JpaAuditingConfig.java
+++ src/main/java/com/loginwebservice/loginwebservice/jpa/config/JpaAuditingConfig.java
@@ -1,18 +1,10 @@
 package com.loginwebservice.loginwebservice.jpa.config;
 
-import com.loginwebservice.loginwebservice.jpa.AuditorAwareImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+@EnableJpaAuditing()
 public class JpaAuditingConfig {
-
-    @Bean
-    public AuditorAware<Long> auditorProvider(){
-        return new AuditorAwareImpl();
-    }
 
 }
