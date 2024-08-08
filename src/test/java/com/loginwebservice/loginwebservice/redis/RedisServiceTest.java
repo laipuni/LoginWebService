c0b65ae94d10443c5e30836bd97aaa6b041a7761
laipuni <2tgb02023@gmail.com>
2024-08-08 17:42:42 +0900

    test : RedisService를 테스트하는 RedisServiceTest 생성

---
+++ src/test/java/com/loginwebservice/loginwebservice/redis/RedisServiceTest.java
@@ -0,0 +1,83 @@
+package com.loginwebservice.loginwebservice.redis;
+
+import com.loginwebservice.loginwebservice.IntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class RedisServiceTest extends IntegrationTest {
+
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @DisplayName("해당 키로 저장된 값을 조회한다.")
+    @Test
+    void getData(){
+        //given
+        String expectedKey = "예상키";
+        String expectedValue = "예상값";
+
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        operations.set(expectedKey,expectedValue);
+        //when
+        String resultValue = redisService.getData(expectedKey);
+        //then
+        assertThat(resultValue).isEqualTo(expectedValue);
+    }
+
+    @DisplayName("해당 키로 저장된 값이 있는지 확인한다.")
+    @Test
+    void existData(){
+        String expectedKey = "예상키";
+        String expectedValue = "예상값";
+
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        operations.set(expectedKey,expectedValue);
+        //when
+        Boolean result = redisService.existData(expectedKey);
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("키와 값을 지정된 시간 후 삭제된다.")
+    @Test
+    void setDataExpire() throws InterruptedException {
+        String expectedKey = "예상키";
+        String expectedValue = "예상값";
+        long duration = 1;
+
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        operations.set(expectedKey,expectedValue);
+        //when
+        redisService.setDataExpire(expectedKey,expectedKey,duration);
+        Thread.sleep(1000 * duration);
+
+        //then
+        Boolean result = redisTemplate.hasKey(expectedKey);
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("key값을 받아 해당 키와 값를 삭제한다.")
+    @Test
+    void deleteData(){
+        String expectedKey = "예상키";
+        String expectedValue = "예상값";
+
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        operations.set(expectedKey,expectedValue);
+        //when
+        redisService.deleteData(expectedKey);
+
+        //then
+        Boolean result = redisTemplate.hasKey(expectedKey);
+        assertThat(result).isFalse();
+    }
+}
