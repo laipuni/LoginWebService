a17cad18e7bbbdf26ae6ffa18c53d89835af40ba
laipuni <2tgb02023@gmail.com>
2024-08-10 17:39:47 +0900

    feat : UserApiControllerTest에 아이디 찾기 인증코드 요청, 아이디 찾기 인증코드 검증 요청 Api 테스트 작성

--- src/test/java/com/loginwebservice/loginwebservice/domain/user/UserApiControllerTest.java
+++ src/test/java/com/loginwebservice/loginwebservice/domain/user/UserApiControllerTest.java
@@ -1,6 +1,9 @@
 package com.loginwebservice.loginwebservice.domain.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loginwebservice.loginwebservice.domain.user.req.UserIdVerifyAuthCodeRequest;
+import com.loginwebservice.loginwebservice.domain.user.request.UserIdSendAuthCodeRequest;
+import com.loginwebservice.loginwebservice.domain.user.response.UserIdVerifyAuthCodeResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,6 +14,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +33,9 @@ class UserApiControllerTest {
 
     @MockBean
     UserService userService;
+
+    @MockBean
+    UserHelpService userHelpService;
 
     @DisplayName("중복 체크할 아이디를 받아 중복된 아이디가 존재하는지 반환한다.")
     @Test
@@ -66,6 +75,232 @@ class UserApiControllerTest {
                 .andExpect(jsonPath("$.status").value("OK"))
                 .andExpect(jsonPath("$.code").value(200))
                 .andExpect(jsonPath("$.data").value(Boolean.TRUE));
+    }
+
+
+    @DisplayName("아이디 인증 코드 요청시, 이름과 이메일이 적절하게 받았을 경우 결과를 반환한다.")
+    @Test
+    void sendAuthCode() throws Exception {
+        //given
+        String expectedName = "김사자";
+        String expectedEmail = "email@email.com";
+
+        UserIdSendAuthCodeRequest request = UserIdSendAuthCodeRequest.builder()
+                .name(expectedName)
+                .email(expectedEmail)
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                post("/api/users/send-id-auth-code")
+                        .with(csrf())
+                        .contentType(APPLICATION_JSON)
+                        .content(data)
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("아이디 인증 코드 요청시, 이름을 입력하지 않은 경우 에러가 발생한다.")
+    @Test
+    void sendAuthCodeWithBlankLoginId() throws Exception {
+        //given
+        String blankName = "";
+        String expectedEmail = "email@email.com";
+
+        UserIdSendAuthCodeRequest request = UserIdSendAuthCodeRequest.builder()
+                .name(blankName)
+                .email(expectedEmail)
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        post("/api/users/send-id-auth-code")
+                                .with(csrf())
+                                .contentType(APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("이름 : 해당 입력은 필수입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+
+    @DisplayName("아이디 인증 코드 요청시, 받은 이메일의 형식에 맞지 않는 경우 에러가 발생한다.")
+    @Test
+    void sendAuthCodeWithInvalidEmail() throws Exception {
+        //given
+        String expectedName = "김사자";
+        String wrongEmail = "wrongEmail";
+
+        UserIdSendAuthCodeRequest request = UserIdSendAuthCodeRequest.builder()
+                .name(expectedName)
+                .email(wrongEmail)
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        post("/api/users/send-id-auth-code")
+                                .with(csrf())
+                                .contentType(APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("이메일 : 이메일 주소가 정확한지 확인해 주세요."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("아이디 인증 코드 검증 요청시, 이름,이메일,인증 코드 적절하게 받았을 경우 결과를 반환한다.")
+    @Test
+    void verifyAuthCode() throws Exception {
+        //given
+        String expectedName = "김사자";
+        String expectedEmail = "email@email.com";
+        String expectedAuthCode = "123456";
+        String mockLoginId = "loginId";
+
+        UserIdVerifyAuthCodeRequest request = UserIdVerifyAuthCodeRequest.builder()
+                .name(expectedName)
+                .email(expectedEmail)
+                .authCode(expectedAuthCode)
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        UserIdVerifyAuthCodeResponse response = UserIdVerifyAuthCodeResponse.builder()
+                .loginId(mockLoginId)
+                .build();
+
+        Mockito.when(
+                    userHelpService.verifyHelpUserIdAuthCode(expectedAuthCode,expectedName,expectedEmail)
+                ).thenReturn(response);
+
+        //when
+        //then
+        mockMvc.perform(
+                        post("/api/users/valid-id-auth-code")
+                                .with(csrf())
+                                .contentType(APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.data.loginId").value(mockLoginId));
+    }
+
+    @DisplayName("아이디 인증 코드 검증 요청시, 이름  받았을 경우 결과를 반환한다.")
+    @Test
+    void verifyAuthCodeWithBlankName() throws Exception {
+        //given
+        String expectedName = " ";
+        String expectedEmail = "email@email.com";
+        String expectedAuthCode = "123456";
+
+        UserIdVerifyAuthCodeRequest request = UserIdVerifyAuthCodeRequest.builder()
+                .name(expectedName)
+                .email(expectedEmail)
+                .authCode(expectedAuthCode)
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        post("/api/users/valid-id-auth-code")
+                                .with(csrf())
+                                .contentType(APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("이름 : 해당 입력은 필수입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+    @DisplayName("아이디 인증 코드 검증 요청시, 받은 이메일의 형식에 맞지 않는 경우 에러가 발생한다.")
+    @Test
+    void verifyAuthCodeWithInvalidEmail() throws Exception {
+        //given
+        String expectedName = "김사자";
+        String expectedEmail = "wrongEmail";
+        String expectedAuthCode = "123456";
+
+        UserIdVerifyAuthCodeRequest request = UserIdVerifyAuthCodeRequest.builder()
+                .name(expectedName)
+                .email(expectedEmail)
+                .authCode(expectedAuthCode)
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        post("/api/users/valid-id-auth-code")
+                                .with(csrf())
+                                .contentType(APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("이메일 : 이메일 주소가 정확한지 확인해 주세요."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("아이디 인증 코드 검증 요청시, 인증 코드가 없을 경우 에러가 발생한다.")
+    @Test
+    void verifyAuthCodeWithBlankAuthCode() throws Exception {
+        //given
+        String expectedName = "김사자";
+        String expectedEmail = "email@email.com";
+        String expectedAuthCode = "";
+
+        UserIdVerifyAuthCodeRequest request = UserIdVerifyAuthCodeRequest.builder()
+                .name(expectedName)
+                .email(expectedEmail)
+                .authCode(expectedAuthCode)
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(
+                        post("/api/users/valid-id-auth-code")
+                                .with(csrf())
+                                .contentType(APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("인증 코드 : 해당 입력은 필수입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
     }
 
 }
