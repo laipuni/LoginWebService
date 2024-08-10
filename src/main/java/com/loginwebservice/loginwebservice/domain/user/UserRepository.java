7062fa040c12192d0a1871cb3ff8dfb6e8ecf9ca
laipuni <2tgb02023@gmail.com>
2024-08-10 17:05:40 +0900

    feat : 해당 이메일과 이름을 가진 회원이 존재를 확인하는 기능, 조회하는 기능 개발

--- src/main/java/com/loginwebservice/loginwebservice/domain/user/UserRepository.java
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.loginwebservice.loginwebservice.domain.user;
 
+import com.loginwebservice.loginwebservice.domain.user.response.UserIdVerifyAuthCodeResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +14,12 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     public boolean existsUserByLoginId (String loginId);
 
+    public boolean existsUserByEmailAndName(String email, String name);
+
     public Optional<User> findUserByLoginId(String loginId);
+
+    @Query("select new com.loginwebservice.loginwebservice.domain.user.response.UserIdVerifyAuthCodeResponse(u.loginId) " +
+            "from User u " +
+            "where u.name =:name and u.email=:email")
+    public Optional<UserIdVerifyAuthCodeResponse> findByNameAndEmail(@Param("name") String name, @Param("email") String email);
 }
