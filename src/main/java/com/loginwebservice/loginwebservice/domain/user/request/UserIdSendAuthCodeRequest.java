e975d81c2a2d332221e8165317bc5c9d7e7c2371
laipuni <2tgb02023@gmail.com>
2024-08-10 17:34:50 +0900

    feat : 유저 아이디 찾기 인증 Request Dto UserIdSendAuthCodeRequest개발

---
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/request/UserIdSendAuthCodeRequest.java
@@ -0,0 +1,22 @@
+package com.loginwebservice.loginwebservice.domain.user.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserIdSendAuthCodeRequest {
+    @NotBlank(message = "이름 : 해당 입력은 필수입니다.")
+    private String name;
+
+    @Pattern(
+            regexp = "\\w+@\\w+\\.\\w+(\\.\\w+)?",
+            message = "이메일 : 이메일 주소가 정확한지 확인해 주세요."
+    )
+    private String email;
+}
