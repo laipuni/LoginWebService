eb0bbbea614766d0728ef1303ed1a4e653803625
laipuni <2tgb02023@gmail.com>
2024-08-13 12:00:21 +0900

    feat : CustomUserDetailService에 HttpSession 참조 추가 및 session에 "user" 저장 로직 추가

--- src/main/java/com/loginwebservice/loginwebservice/security/formLogin/CustomUserDetailService.java
+++ src/main/java/com/loginwebservice/loginwebservice/security/formLogin/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package com.loginwebservice.loginwebservice.security.formLogin;
 import com.loginwebservice.loginwebservice.domain.user.User;
 import com.loginwebservice.loginwebservice.domain.user.UserRepository;
 import com.loginwebservice.loginwebservice.security.formLogin.domain.SecurityUser;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,11 +15,14 @@ import org.springframework.stereotype.Component;
 public class CustomUserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final HttpSession httpSession;
 
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         User user = userRepository.findUserByLoginId(username)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 아이디의 유저가 존재하지 않습니다."));
+
+        httpSession.setAttribute("user",new SecurityUser(user));
 
         return new SecurityUser(user);
     }
