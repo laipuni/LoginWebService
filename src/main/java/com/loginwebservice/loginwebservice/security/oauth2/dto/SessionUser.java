d83fa63a2f02a954dfd222254defa3e72d895b12
laipuni <2tgb02023@gmail.com>
2024-08-13 11:56:06 +0900

    feat : SessionUser에 필드 userSeq 추가
    AuditorAwareImpl에서 생성,수정시 사용할 user_Id이다

--- src/main/java/com/loginwebservice/loginwebservice/security/oauth2/dto/SessionUser.java
+++ src/main/java/com/loginwebservice/loginwebservice/security/oauth2/dto/SessionUser.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 @Getter
 public class SessionUser {
 
+    private Long userSeq;
     private String name;
     private String email;
     private String picture;
 
     public SessionUser(final User user) {
+        this.userSeq = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
