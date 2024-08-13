4cbf1bfbf153ced664c0d4a6297548322469ee57
laipuni <2tgb02023@gmail.com>
2024-08-13 12:07:14 +0900

    feat : AuditorAwareImp에 httpsession 담긴 sessionUser 조회한 뒤 반환하는 로직추가

--- src/main/java/com/loginwebservice/loginwebservice/jpa/AuditorAwareImpl.java
+++ src/main/java/com/loginwebservice/loginwebservice/jpa/AuditorAwareImpl.java
@@ -1,31 +1,29 @@
 package com.loginwebservice.loginwebservice.jpa;
 
-import com.loginwebservice.loginwebservice.security.formLogin.domain.SecurityUser;
+import com.loginwebservice.loginwebservice.security.oauth2.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
 @Slf4j
 public class AuditorAwareImpl implements AuditorAware<Long> {
 
+    private HttpSession httpSession;
+    public AuditorAwareImpl(final HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+
     @Override
     public Optional<Long> getCurrentAuditor() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication == null || authentication instanceof AnonymousAuthenticationToken){
-            log.debug("Not found Authentication");
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user == null){
+            log.debug("Not found Session");
             return null;
         }
 
-        if(!authentication.isAuthenticated()){
-            log.debug("Not Authentication");
-            return null;
-        }
-
-        SecurityUser user = (SecurityUser) authentication.getPrincipal();
+        //OAuth2 유저는 principal이 SecurityUser가 아닌 DefaultOAuth2User이다.
         return Optional.of(user.getUserSeq());
     }
 }
--- src/main/java/com/loginwebservice/loginwebservice/jpa/config/JpaAuditingConfig.java
+++ src/main/java/com/loginwebservice/loginwebservice/jpa/config/JpaAuditingConfig.java
@@ -1,6 +1,8 @@
 package com.loginwebservice.loginwebservice.jpa.config;
 
 import com.loginwebservice.loginwebservice.jpa.AuditorAwareImpl;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -9,9 +11,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfig {
+
+    @Autowired
+    HttpSession httpSession;
+
     @Bean
     public AuditorAware<Long> auditorProvider(){
-        return new AuditorAwareImpl();
+        return new AuditorAwareImpl(httpSession);
     }
 
 }
