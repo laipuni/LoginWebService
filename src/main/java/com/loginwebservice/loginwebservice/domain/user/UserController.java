c0244f316883ffdbd4d48dee6a30761be3597e79
laipuni <2tgb02023@gmail.com>
2024-08-15 14:40:24 +0900

    feat : 비밀번호 찾기 Ui조회 api 구현

--- src/main/java/com/loginwebservice/loginwebservice/domain/user/UserController.java
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/UserController.java
@@ -1,6 +1,7 @@
 package com.loginwebservice.loginwebservice.domain.user;
 
 import com.loginwebservice.loginwebservice.domain.user.request.UserAddRequest;
+import com.loginwebservice.loginwebservice.redis.RedisService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -10,12 +11,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final RedisService redisService;
 
     @GetMapping("/users/join")
     public String joinForm(@ModelAttribute(name = "user") UserAddRequest request){
@@ -50,6 +53,44 @@ public class UserController {
 
     @GetMapping("/users/help-id")
     public String findIdForm(){
-        return "user/userIdHelpForm";
+        return "user/help/userIdHelpForm";
+    }
+
+    @GetMapping("/users/help-password")
+    public String findPasswordForm(
+            @RequestParam(defaultValue = "viewLoginIdInput",name = "menu") String menu,
+            @RequestParam(required = false,name = "token_help") String token,
+            Model model
+    ){
+        if(token != null){
+            return viewNextFormBy(token,menu,model);
+        }
+
+        return "user/help/userLoginIdInputForm";
+    }
+
+    private String viewNextFormBy(final String token, final String menu, final Model model) {
+        //유효하지 않을 경우 해당 세션이 만료됐다는 메세지와 함께 login화면으로 가도록
+        if(!redisService.existData(token)){
+            return "redirect:/users/help-password";
+        }
+
+        model.addAttribute("token_help",token);
+
+        if(menu.equals("viewPasswordAuth")){
+            //인증 화면
+            return "user/help/userPasswordAuthForm";
+        } else if(menu.equals("viewPasswordInput")){
+            //비밀번호 재설정
+            return createViewUserPasswordInputForm(token, model);
+        } else{
+            return "user/help/userLoginIdInputForm";
+        }
+    }
+
+    private String createViewUserPasswordInputForm(final String token, final Model model) {
+        String loginId = redisService.getData(token);
+        model.addAttribute("loginId",loginId);
+        return "user/help/userPasswordInputForm";
     }
 }
