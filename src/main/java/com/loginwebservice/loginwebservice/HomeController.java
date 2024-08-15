f0021c25535b898d5f7f08d5d4a0a078c0b422a8
laipuni <2tgb02023@gmail.com>
2024-08-15 13:52:15 +0900

    feat : HomeController에 session에 저장된 정보를 가져오는 로직 제거

--- src/main/java/com/loginwebservice/loginwebservice/HomeController.java
+++ src/main/java/com/loginwebservice/loginwebservice/HomeController.java
@@ -1,7 +1,5 @@
 package com.loginwebservice.loginwebservice;
 
-import com.loginwebservice.loginwebservice.security.oauth2.dto.SessionUser;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final HttpSession httpSession;
-
     @GetMapping("/")
     public String home(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-        if(user != null){
-            model.addAttribute("user",user);
-        }
-
         return "home";
     }
 }
