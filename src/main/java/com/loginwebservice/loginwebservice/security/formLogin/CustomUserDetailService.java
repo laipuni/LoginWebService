c1cf1bb1639b55954ed650bb02d12596156afcd6
laipuni <2tgb02023@gmail.com>
2024-08-15 13:43:50 +0900

    feat : CustomUserDetailService에 세션에 저장할 객체를 SecurityUser -> SessionUser변경

--- src/main/java/com/loginwebservice/loginwebservice/security/formLogin/CustomUserDetailService.java
+++ src/main/java/com/loginwebservice/loginwebservice/security/formLogin/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package com.loginwebservice.loginwebservice.security.formLogin;
 import com.loginwebservice.loginwebservice.domain.user.User;
 import com.loginwebservice.loginwebservice.domain.user.UserRepository;
 import com.loginwebservice.loginwebservice.security.formLogin.domain.SecurityUser;
+import com.loginwebservice.loginwebservice.security.oauth2.dto.SessionUser;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,7 +23,7 @@ public class CustomUserDetailService implements UserDetailsService {
         User user = userRepository.findUserByLoginId(username)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 아이디의 유저가 존재하지 않습니다."));
 
-        httpSession.setAttribute("user",new SecurityUser(user));
+        httpSession.setAttribute("user",new SessionUser(user));
 
         return new SecurityUser(user);
     }
