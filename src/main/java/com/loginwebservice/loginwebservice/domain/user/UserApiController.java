dfd3caf7a308cd21db1d888b8bfe64be08c81766
laipuni <2tgb02023@gmail.com>
2024-08-15 14:42:12 +0900

    feat : 비밀번호 찾기 api 구현
    isExistLoginId(): 비밀번호를 찾을 계정이 존재하는지 확인 api
    sendPasswordAuthCode(): 비밀번호 인증코드 요청 api
    validPasswordAuthCode(): 비밀번호 인증코드 검증 요청 api
    resetPassword(): 비밀번호 재설정 api

--- src/main/java/com/loginwebservice/loginwebservice/domain/user/UserApiController.java
+++ src/main/java/com/loginwebservice/loginwebservice/domain/user/UserApiController.java
@@ -1,44 +1,75 @@
 package com.loginwebservice.loginwebservice.domain.user;
 
 import com.loginwebservice.loginwebservice.api.ApiResponse;
-import com.loginwebservice.loginwebservice.domain.user.req.UserIdVerifyAuthCodeRequest;
-import com.loginwebservice.loginwebservice.domain.user.request.UserIdSendAuthCodeRequest;
-import com.loginwebservice.loginwebservice.domain.user.response.UserIdVerifyAuthCodeResponse;
+import com.loginwebservice.loginwebservice.domain.user.request.*;
+import com.loginwebservice.loginwebservice.domain.user.response.LoginIdValidationResponse;
+import com.loginwebservice.loginwebservice.domain.user.response.PasswordAuthCodeValidResponse;
+import com.loginwebservice.loginwebservice.domain.user.response.LoginIdSearchResponse;
+import com.loginwebservice.loginwebservice.redis.RedisService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserApiController {
 
     private final UserService userService;
     private final UserHelpService userHelpService;
-
-    @GetMapping("/users/join/check-login-id")
+    private final RedisService redisService;
+    @GetMapping("/join/check-login-id")
     public ApiResponse<Boolean> isExistSameLoginIdUser(@RequestParam("loginId") String loginId){
         boolean result = userService.isExistSameLoginIdUSer(loginId);
         return ApiResponse.of(HttpStatus.OK,result);
     }
 
-    @GetMapping("/users/join/check-user-name")
+    @GetMapping("/join/check-user-name")
     public ApiResponse<Boolean> isExistSameUserName(@RequestParam("userName") String userName){
         boolean result = userService.isExistSameUserName(userName);
         return ApiResponse.of(HttpStatus.OK,result);
     }
 
-    @PostMapping("/users/send-id-auth-code")
-    public ApiResponse<Object> sendAuthCode(@Valid @RequestBody UserIdSendAuthCodeRequest request){
+    @PostMapping("/send-id-auth-code")
+    public ApiResponse<Object> sendLoginIdAuthCode(@Valid @RequestBody LoginIdAuthCodeSendRequest request){
         userHelpService.helpUserId(request.getName(),request.getEmail());
         return ApiResponse.of(HttpStatus.OK,null);
     }
 
-    @PostMapping("/users/valid-id-auth-code")
-    public ApiResponse<UserIdVerifyAuthCodeResponse> verifyAuthCode(@Valid @RequestBody UserIdVerifyAuthCodeRequest request){
-        UserIdVerifyAuthCodeResponse response = userHelpService.verifyHelpUserIdAuthCode(request.getAuthCode(), request.getName(), request.getEmail());
+    @PostMapping("/valid-id-auth-code")
+    public ApiResponse<LoginIdValidationResponse> validLoginIdAuthCode(@Valid @RequestBody LoginIdAuthCodeValidRequest request){
+        LoginIdValidationResponse response = userHelpService.validHelpUserIdAuthCode(request.getAuthCode(), request.getName(), request.getEmail());
         return ApiResponse.of(HttpStatus.OK,response);
     }
 
+    @GetMapping("/search-loginId")
+    public ApiResponse<LoginIdSearchResponse> isExistLoginId(@RequestParam("loginId") String loginId){
+        String helpToken = UUID.randomUUID().toString();
+        return ApiResponse.of(HttpStatus.OK, userHelpService.searchLoginId(loginId,helpToken));
+    }
+
+    @PostMapping("/send-password-auth-code")
+    public ApiResponse<Object> sendPasswordAuthCode(@Valid @RequestBody PasswordAuthCodeSendRequest request){
+        userHelpService.helpUserPassword(request.getName(), request.getEmail());
+        return ApiResponse.of(HttpStatus.OK,null);
+    }
+    
+    @PostMapping("/valid-password-auth-code")
+    public ApiResponse<PasswordAuthCodeValidResponse> validPasswordAuthCode(@Valid @RequestBody PasswordAuthCodeValidRequest request){
+        PasswordAuthCodeValidResponse response = userHelpService.validPasswordAuthCode(
+                request.getAuthCode(), request.getName(), request.getEmail()
+        );
+        return ApiResponse.of(HttpStatus.OK, response);
+    }
+
+    @PostMapping("/reset-password")
+    public ApiResponse<Object> resetPassword(@Valid @RequestBody PasswordResetRequest request){
+        userHelpService.resetPassword(
+                request.getToken(),request.getPassword()
+        );
+        return ApiResponse.of(HttpStatus.OK,null);
+    }
 }
