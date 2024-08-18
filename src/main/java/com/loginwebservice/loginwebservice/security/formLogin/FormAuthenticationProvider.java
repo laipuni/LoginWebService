918a89c9182ff9fd158c9dd39df51438c33807ef
laipuni <2tgb02023@gmail.com>
2024-08-18 19:08:44 +0900

    feat : FormAuthenticationProvider에 로그인 잠금상태인 계정은 로그인을 막는 로직 추가

--- src/main/java/com/loginwebservice/loginwebservice/security/formLogin/FormAuthenticationProvider.java
+++ src/main/java/com/loginwebservice/loginwebservice/security/formLogin/FormAuthenticationProvider.java
@@ -1,6 +1,10 @@
 package com.loginwebservice.loginwebservice.security.formLogin;
 
+import com.loginwebservice.loginwebservice.security.formLogin.exception.LoginFailException;
+import com.loginwebservice.loginwebservice.security.formLogin.service.CustomUserDetailService;
+import com.loginwebservice.loginwebservice.security.formLogin.service.LoginFailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,33 +14,36 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class FormAuthenticationProvider implements AuthenticationProvider {
 
     private final CustomUserDetailService userDetailService;
     private final PasswordEncoder passwordEncoder;
+    private final LoginFailService loginFailService;
 
     @Override
     public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
-        String userName = (String) authentication.getPrincipal();
+        String loginId = (String) authentication.getPrincipal();
         String password = (String) authentication.getCredentials();
 
-        UserDetails user = userDetailService.loadUserByUsername(userName);
+        if(loginFailService.hasLoginLock(loginId)){
+            throw new LoginFailException("해당 계정은 5번이상 로그인 시도 했습니다.");
+        }
+
+        UserDetails user = userDetailService.loadUserByUsername(loginId);
 
         if(!passwordEncoder.matches(password,user.getPassword())){
             throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
         }
 
-        return new UsernamePasswordAuthenticationToken(
-                user,
-                null,
-                user.getAuthorities()
-        );
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
     }
 
     @Override
     public boolean supports(final Class<?> authentication) {
         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
+
 }
