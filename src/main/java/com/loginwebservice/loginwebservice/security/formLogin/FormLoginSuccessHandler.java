72a76a0cefb6bcd69bf06ff798e997722fd46e1c
laipuni <2tgb02023@gmail.com>
2024-08-18 19:07:54 +0900

    feat : FormLoginSuccessHandler에 로그인 성공했을 때, 로그인 실패 횟수를 초기화하는 로직 추가

--- src/main/java/com/loginwebservice/loginwebservice/security/formLogin/FormLoginSuccessHandler.java
+++ src/main/java/com/loginwebservice/loginwebservice/security/formLogin/FormLoginSuccessHandler.java
@@ -1,8 +1,10 @@
 package com.loginwebservice.loginwebservice.security.formLogin;
 
+import com.loginwebservice.loginwebservice.security.formLogin.service.LoginFailService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
@@ -15,14 +17,18 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static com.loginwebservice.loginwebservice.security.config.SecurityConfig.USERNAME_PARAMETER;
+
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class FormLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     public static final String DEFAULT_URL = "/";
     private RequestCache requestCache = new HttpSessionRequestCache();
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
+    private final LoginFailService loginFailService;
     @Override
     public void onAuthenticationSuccess(
             final HttpServletRequest request,
@@ -30,7 +36,8 @@ public class FormLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandl
             final Authentication authentication
     ) throws IOException, ServletException {
         SavedRequest savedRequest = requestCache.getRequest(request, response);
-
+        //로그인 성공시 로그인 시도 횟수 초기화
+        loginFailService.resetLoginFailCount(request.getParameter(USERNAME_PARAMETER));
         if(savedRequest != null){
             String redirectUrl = savedRequest.getRedirectUrl();
             redirectStrategy.sendRedirect(request,response,redirectUrl);
