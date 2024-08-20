8dc2e518fd9389ce103403bbb6776c417a0f2cff
laipuni <2tgb02023@gmail.com>
2024-08-20 14:35:06 +0900

    feat : CustomUserDetailService
    로그인을 시도한 계정이 아직 인증을 하지않을 경우 NotAuthenticationUserException 예외가 발생한다.

--- src/main/java/com/loginwebservice/loginwebservice/security/formLogin/service/CustomUserDetailService.java
+++ src/main/java/com/loginwebservice/loginwebservice/security/formLogin/service/CustomUserDetailService.java
@@ -3,6 +3,7 @@ package com.loginwebservice.loginwebservice.security.formLogin.service;
 import com.loginwebservice.loginwebservice.domain.user.User;
 import com.loginwebservice.loginwebservice.domain.user.UserRepository;
 import com.loginwebservice.loginwebservice.security.formLogin.domain.SecurityUser;
+import com.loginwebservice.loginwebservice.security.formLogin.exception.NotAuthenticationUserException;
 import com.loginwebservice.loginwebservice.security.oauth2.dto.SessionUser;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,10 @@ public class CustomUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         User user = userRepository.findUserByLoginId(username)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 아이디의 유저가 존재하지 않습니다."));
+
+        if(!user.isAuthenticated()){
+            throw new NotAuthenticationUserException("해당 계정은 인증하지 않았습니다. 이메일을 확인해주세요");
+        }
 
         httpSession.setAttribute("user",new SessionUser(user));
 
