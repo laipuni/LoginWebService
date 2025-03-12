a8aa0a0ceea842d025bfa23505c83213a111e46d
laipuni <2tgb02023@gmail.com>
2025-03-12 12:17:06 +0900

    feat : redis 연결 예외 핸들링 및 예외를 개발자 메일 알림 추가

--- src/main/java/com/loginwebservice/loginwebservice/api/ExceptionController.java
+++ src/main/java/com/loginwebservice/loginwebservice/api/ExceptionController.java
@@ -1,17 +1,29 @@
 package com.loginwebservice.loginwebservice.api;
 
+import com.loginwebservice.loginwebservice.Email.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ExceptionController {
+
+    @Value("${spring.mail.username}")
+    private String toDeveloper;
+
+    private final EmailService emailService;
 
     @ExceptionHandler(value = BindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrorResponse<Object> bindException(BindException exception){
+        log.trace("[bindException] msg = {}", exception.getMessage());
         return ApiErrorResponse.of(
                 HttpStatus.BAD_REQUEST,
                 exception.getBindingResult()
@@ -24,6 +36,23 @@ public class ExceptionController {
     @ExceptionHandler(value = IllegalArgumentException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ApiErrorResponse<Object> IllegalArgumentException(IllegalArgumentException exception){
+        log.trace("[IllegalArgumentException] msg = {}", exception.getMessage());
+        return ApiErrorResponse.of(
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                null
+        );
+    }
+
+    @ExceptionHandler(value = IllegalStateException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse<Object> IllegalStateException(IllegalStateException exception){
+        log.warn("[IllegalStateException] msg = {}, cause = {}", exception.getMessage(), exception.getCause());
+        emailService.sendEmail(
+                toDeveloper,
+                exception.getCause().getLocalizedMessage(),
+                exception.getMessage()
+        );
         return ApiErrorResponse.of(
                 HttpStatus.BAD_REQUEST,
                 exception.getMessage(),
--- src/main/java/com/loginwebservice/loginwebservice/redis/RedisRepository.java
+++ src/main/java/com/loginwebservice/loginwebservice/redis/RedisRepository.java
@@ -1,35 +1,54 @@
 package com.loginwebservice.loginwebservice.redis;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.concurrent.Callable;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisRepository {
 
     private final StringRedisTemplate template;
 
-    public String getData(String key){
-        ValueOperations<String, String> operations = template.opsForValue();
-        return operations.get(key);
+    public String getData(String key) {
+        return (String) handleException(() -> {
+            ValueOperations<String, String> operations = template.opsForValue();
+            return operations.get(key);
+        });
     }
 
-    public Boolean existData(String key){
-        return template.hasKey(key);
+    public Boolean existData(String key) {
+        return (Boolean) handleException(() -> Boolean.TRUE.equals(template.hasKey(key)));
     }
 
-    public void setDataExpire(String key, String value, long duration){
-        ValueOperations<String, String> operations = template.opsForValue();
-        Duration expireDuration = Duration.ofSeconds(duration);
-        operations.set(key, value, expireDuration);
+    public void setDataExpire(String key, String value, long duration) {
+        handleException(() -> {
+            ValueOperations<String, String> operations = template.opsForValue();
+            Duration expireDuration = Duration.ofSeconds(duration);
+            operations.set(key, value, expireDuration);
+            return null;
+        });
     }
 
-    public void deleteData(String key){
-        template.delete(key);
+    public Boolean deleteData(String key) {
+        return (Boolean) handleException(() -> template.delete(key));
+    }
+
+    private Object handleException(Callable<?> callable) {
+        try {
+            return callable.call();
+        } catch (RedisConnectionFailureException e) {
+            throw new IllegalStateException("서버에 문제가 생겨 해당 서비스를 이용할 수 없습니다. 죄송합니다.", e);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
 }
